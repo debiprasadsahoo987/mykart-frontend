0e0531b4013d6a7afde4633ad03e88f095daf729
Debiprasad <debiprasadsahoo987@gmail.com>
2025-09-17 23:56:56 +0530

    working on stripe payments

--- src/components/checkout/StripePayment.jsx
+++ src/components/checkout/StripePayment.jsx
@@ -1,7 +1,24 @@
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
 import React from "react";
+import { useSelector } from "react-redux";
+import PaymentForm from "./PaymentForm";
+
+const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLISHABLE_KEY);
 
 const StripePayment = () => {
-  return <div>StripePayment</div>;
+  const { clientSecret } = useSelector((state) => state.auth);
+  const { totalPrice } = useSelector((state) => state.cart);
+  const { isLoading, errorMessage } = useSelector((state) => state.errors);
+  return (
+    <>
+      {clientSecret && (
+        <Elements stripe={stripePromise} options={{ clientSecret }}>
+          <PaymentForm clientSecret={clientSecret} totalPrice={totalPrice} />
+        </Elements>
+      )}
+    </>
+  );
 };
 
 export default StripePayment;
